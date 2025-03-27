56b7c5782cbd98239ef1c26d7e00fc67b0f4eadd
Marcelo Maia Sobral <msobral@gmail.com>
2025-03-27 16:23:05 -0300

    Update Fila.java

--- src/esd/Fila.java
+++ src/esd/Fila.java
@@ -2,7 +2,16 @@ package esd;
 
 public class Fila <T> {
 
-    public Fila(int capacity) {
+    // aqui serão armazenados os dados em fila
+    // note que outros atributos da classe Fila serão necessários para implementar a fila !
+    T[] mem;
+
+    // esta anotação é necessária para que o compilador Java aceite criar a área de armazenamento
+    @SuppressWarnings("unchecked")
+    public Fila(int cap) {
+        // a área de armazenamento da fila precisa deste truque para ser criada
+        // não é possível criar diretamente com new T[capacity], uma vez que o tipo T é desconhecido !
+        mem = (T[])new Object[cap];
     }
 
     public int comprimento() {
