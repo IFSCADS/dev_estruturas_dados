14985628eda14bc7db2f2a6d65bf10ed11907df8
Marcelo Maia Sobral <msobral@gmail.com>
2025-03-27 14:53:27 -0300

    Update Fila.java

--- src/esd/Fila.java
+++ src/esd/Fila.java
@@ -1,24 +1,27 @@
 package esd;
 
-public class Fila {
+public class Fila <T> {
 
     public Fila() {
     }
 
-    public int length() {
+    public int comprimento() {
     }
 
     public int capacidade() {
     }
 
-    public void adiciona(Object algo) {
+    public void adiciona(T algo) {
     }
 
-    public Object remove() {
+    public T remove() {
     }
 
-    public Object frente() {
+    public T frente() {
     }
+
+    public T fim() {
+    }    
 
     public boolean estaVazia() {
     }
