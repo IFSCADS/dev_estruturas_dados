f63a1e9c7af8f3da737a29df635d0335ee52d95b
Marcelo Maia Sobral <msobral@gmail.com>
2025-03-27 16:26:35 -0300

    Update TestFilaSimples.java

--- test/TestFilaSimples.java
+++ test/TestFilaSimples.java
@@ -12,19 +12,19 @@ class TestFilaSimples {
     @org.junit.jupiter.api.Test
     @DisplayName("Testa criação de fila: deve estar vazia ao ser criada")
     void criaFila() throws InterruptedException, IOException {
-        Fila q = new Fila(5);
+        Fila<Integer> q = new Fila<>(5);
         assert(q.estaVazia());
     }
 
     @org.junit.jupiter.api.Test
     @DisplayName("Testa enfileirar valores")
     void adiciona() throws InterruptedException, IOException {
-        Fila q = new Fila(5);
+        Fila<Integer> q = new Fila<>(5);
 
         for (int j=0; j < 5; j++) {
             var val = Integer.valueOf(j);
             q.adiciona(val);
-            assert(q.length() == j+1);
+            assert(q.comprimento() == j+1);
         }
 
         assert(! q.estaVazia());
@@ -33,13 +33,13 @@ class TestFilaSimples {
     @org.junit.jupiter.api.Test
     @DisplayName("Testa enfileirar valores e acessar frente")
     void frente() throws InterruptedException, IOException {
-        Fila q = new Fila(5);
+        Fila<Integer> q = new Fila<>(5);
 
         var primeiro = Integer.valueOf(0);
         for (int j=0; j < 5; j++) {
             var val = Integer.valueOf(j);
             q.adiciona(val);
-            var dado = (Integer)q.frente();
+            var dado = q.frente();
             assert(dado.equals(primeiro));
         }
     }
@@ -47,7 +47,7 @@ class TestFilaSimples {
     @org.junit.jupiter.api.Test
     @DisplayName("Testa enfileirar valores a partir de um posição diferente do início da área de armazenamento, para evidenciar ser fila circular")
     void adiciona_circular() throws InterruptedException, IOException {
-        Fila q = new Fila(5);
+        Fila<Integer> q = new Fila<>(5);
 
         q.adiciona(Integer.valueOf(3));
         q.adiciona(Integer.valueOf(3));
@@ -57,12 +57,12 @@ class TestFilaSimples {
         for (int j=0; j < 5; j++) {
             var val = Integer.valueOf(j);
             q.adiciona(val);
-            assert(q.length() == j+1);
+            assert(q.comprimento() == j+1);
         }
 
         for (int j=0; j < 5; j++) {
             assert(! q.estaVazia());
-            var val = (Integer)q.remove();
+            var val = q.remove();
             assert(val.equals(Integer.valueOf(j)));
         }
 
@@ -72,7 +72,7 @@ class TestFilaSimples {
     @org.junit.jupiter.api.Test
     @DisplayName("Testa desenfileirar valores")
     void remove() throws InterruptedException, IOException {
-        Fila q = new Fila(5);
+        Fila<Integer> q = new Fila<>(5);
 
         for (int j=0; j < 5; j++) {
             var val = Integer.valueOf(j);
@@ -81,7 +81,7 @@ class TestFilaSimples {
 
         for (int j=0; j < 5; j++) {
             assert(! q.estaVazia());
-            var val = (Integer)q.remove();
+            var val = q.remove();
             assert(val.equals(Integer.valueOf(j)));
         }
 
@@ -91,7 +91,7 @@ class TestFilaSimples {
     @org.junit.jupiter.api.Test
     @DisplayName("Testa acessar frente de fila vazia")
     void frente_vazia() throws InterruptedException, IOException {
-        Fila q = new Fila(5);
+        Fila<Integer> q = new Fila<>(5);
 
         assertThrows(IndexOutOfBoundsException.class, () -> q.frente(), "acessar frente de fila vazia deve disparar uma exceção IndexOutOfBoundsException");
     }    
@@ -99,7 +99,7 @@ class TestFilaSimples {
     @org.junit.jupiter.api.Test
     @DisplayName("Testa enfileirar em fila cheia")
     void fila_cheia() throws InterruptedException, IOException {
-        Fila q = new Fila(5);
+        Fila<Integer> q = new Fila<>(5);
 
         for (int j=0; j < 5; j++) {
             var val = Integer.valueOf(j);
@@ -112,7 +112,7 @@ class TestFilaSimples {
     @org.junit.jupiter.api.Test
     @DisplayName("Testa desenfileirar de fila vazia")
     void fila_vazia() throws InterruptedException, IOException {
-        Fila q = new Fila(5);
+        Fila<Integer> q = new Fila<>(5);
 
         assertThrows(IndexOutOfBoundsException.class, () -> q.remove(), "desenfileirar de fila vazia deve disparar uma exceção IndexOutOfBoundsException");
     }        
